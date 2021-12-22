f987dd32beb6ba71bd92d8f1225450cbcd5585db
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2021-12-22 01:27:35 +0100

    add res

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/", function () {
+app.get("/", function (req, res) {
   res.send("IT Worred");
 });
 app.listen(process.env.PORT || 5000);
