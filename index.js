fa7c94218f2e420f71fdba1b85d2d472a573f212
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2021-12-22 01:19:09 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("is running");
+const express = require("express");
+const app = express();
+app.get("/", function () {
+  res.send("IT Worred");
+});
+app.listen(process.env.PORT || 5000);
+module.exports = app;
